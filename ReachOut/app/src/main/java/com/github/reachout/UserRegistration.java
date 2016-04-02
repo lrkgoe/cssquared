f098d9b1d53dd1bf9e9d2f0f0a769da403e3684c
william <wcheng47@gatech.edu>
2016-04-02 12:42:26 -0400

    Added templates of every activity that is needed to be implemented to the reachout.

--- ReachOut/app/src/main/java/com/github/reachout/UserRegistration.java
+++ ReachOut/app/src/main/java/com/github/reachout/UserRegistration.java
@@ -3,12 +3,11 @@ package com.github.reachout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class UserRegistration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_registration);
     }
-
 }
