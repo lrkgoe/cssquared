28f659c62c86f8d2329c2059e4fc5da8f07a2504
william <wcheng47@gatech.edu>
2016-04-02 15:33:43 -0400

    Added

--- ReachOut/app/src/main/java/com/github/reachout/MakeEventActivity.java
+++ ReachOut/app/src/main/java/com/github/reachout/MakeEventActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MakeEventActivity extends AppCompatActivity {
 
@@ -16,8 +17,15 @@ public class MakeEventActivity extends AppCompatActivity {
         createEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                eventAddress = (EditText) findViewById(R.id.);
-//                summary = ()
+                EditText eventName = (EditText) findViewById(R.id.name_editText);
+                EditText streetNumber = (EditText) findViewById(R.id.street_number_editText);
+                EditText houseAddress = (EditText) findViewById(R.id.house_address_editText);
+                EditText zipCode = (EditText) findViewById(R.id.zip_code_editText);
+                EditText email = (EditText) findViewById(R.id.email_editText);
+                EditText phoneNumber = (EditText) findViewById(R.id.phone_number_editText);
+                EditText summary = (EditText) findViewById(R.id.summary_editText);
+                EditText date = (EditText) findViewById(R.id.date_editText);
+                EditText time = (EditText) findViewById(R.id.time_editText);
             }
 
         });
